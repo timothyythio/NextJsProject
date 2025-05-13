c3ce50bd9572d34863048977d6f9de82a1791cb0
timothyythio <63795701+timothyythio@users.noreply.github.com>
2025-05-13 00:46:36 -0700

    Added Sheet Page

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { APP_NAME } from '@/lib/constants';
-import { Button } from '@/components/ui/button';
-import { ShoppingCart, UserIcon } from 'lucide-react';
-import ModeToggle from './mode-toggle';
+import Menu from './menu';
+
 
 
 const Header = () => {
@@ -24,19 +23,7 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-            <ModeToggle />
-            <Button asChild variant='ghost'>
-                <Link href='/cart'>
-                    <ShoppingCart /> Cart
-                </Link>
-            </Button>
-            <Button asChild>
-                <Link href='/sign-in'>
-                    <UserIcon /> Sign In
-                </Link>
-            </Button>
-        </div>
+        <Menu />
         <div className='hidden md:block'>
         </div>
       </div>
