2f696745e07aca86fe1039c9e140ed6b8b78ee7b
timothyythio <63795701+timothyythio@users.noreply.github.com>
2025-05-12 19:54:30 -0700

    Changed Footer Route

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/shared/header";
-import Footer from "@/components/shared/footer";
+import Footer from "@/components/footer";
 export default function RootLayout({
   children,
 }: Readonly<{
