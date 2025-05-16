a368466a2f86434436707cfb1cd2169d5d221742
timothyythio <63795701+timothyythio@users.noreply.github.com>
2025-05-16 01:00:05 -0700

    Added Products Taken from DB, Displayed in Pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,20 @@
+import { getLatestProducts } from "@/lib/actions/product.actions";
+import ProductList from "@/components/shared/product/product-list";
+
 export const metadata = {
   title: "Home",
 };
-const Homepage = () => {
-  return <>ProStore</>;
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+  return (
+    <>
+      <ProductList
+        data={latestProducts}
+        title="Newest Arrivals"
+        limit={4}
+      ></ProductList>
+    </>
+  );
 };
 
 export default Homepage;
