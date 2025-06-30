5a01a947a97251da4f86c5ed8ffc04f7a082d53e
timothyythio <63795701+timothyythio@users.noreply.github.com>
2025-06-30 01:00:31 -0700

    Created Search Page + Home Page Touchups

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -40,11 +40,17 @@ export async function getAllProducts({
   limit = PAGE_SIZE,
   page,
   category,
+  price,
+  rating,
+  sort,
 }: {
   query: string;
   limit?: number;
   page: number;
   category?: string;
+  price?: string;
+  rating?: string;
+  sort?: string;
 }) {
   // Query filter
   const queryFilter: Prisma.ProductWhereInput =
@@ -60,25 +66,43 @@ export async function getAllProducts({
   // Category filter
   const categoryFilter = category && category !== "all" ? { category } : {};
 
-  // // Price filter
-  // const priceFilter: Prisma.ProductWhereInput =
-  //   price && price !== 'all'
-  //     ? {
-  //         price: {
-  //           gte: Number(price.split('-')[0]),
-  //           lte: Number(price.split('-')[1]),
-  //         },
-  //       }
-  //     : {};
+  // Price filter
+  const priceFilter: Prisma.ProductWhereInput =
+    price && price !== "all"
+      ? {
+          price: {
+            gte: Number(price.split("-")[0]),
+            lte: Number(price.split("-")[1]),
+          },
+        }
+      : {};
+
+  // Rating Filter
+
+  const ratingFilter =
+    rating && rating !== "all"
+      ? {
+          rating: {
+            gte: Number(rating),
+          },
+        }
+      : {};
 
   const data = await prisma.product.findMany({
     where: {
       ...queryFilter,
       ...categoryFilter,
-      // ...priceFilter,
-      // ...ratingFilter,
+      ...priceFilter,
+      ...ratingFilter,
     },
-    orderBy: { createdAt: "desc" },
+    orderBy:
+      sort === "lowest"
+        ? { price: "asc" }
+        : sort === "highest"
+          ? { price: "desc" }
+          : sort === "rating"
+            ? { rating: "desc" }
+            : { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
   });
